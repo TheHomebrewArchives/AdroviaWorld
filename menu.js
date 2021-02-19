ac623570a9f52e9074483b23e4de213d01ea08f7
TheHomebrewArchives <69606809+TheHomebrewArchives@users.noreply.github.com>
2021-02-18 20:05:05 -0500

    Delete menu.js

--- menu.js
+++
@@ -1,5 +0,0 @@
-function renderMenu(){
-  document.getElementById("menu").innerHTML='<ul><li><a href="index.html">Home</a></li><li><div class="dropdown"><button class="dropbtn">Player</button><div class="dropdown-content"><a href="ccreation.html">Character Creation (Dead Link)</a><a href="cultures.html">Cultures (Dead Link)</a><a href="boons.html">Boons</a><a href="classes.html">Classes (Dead Link)</a><a href="upbringings.html">Upbringings (Dead Link)</a></div></div></li><li><div class="dropdown"><button class="dropbtn">References</button><div class="dropdown-content"><a href="spells.html">Spells (Dead Link)</a><a href="combat.html">Combat (Dead Link)</a><a href="spellcasting.html">Spellcasting (Dead Link)</a></div></div></li>';
-}
-
-renderMenu();
